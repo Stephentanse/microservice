dbc2ea49bb005371c0878b958c46de349cf634f5
Stephentanse <1964305379@qq.com>
2019-07-14 19:19:34 +0800

    add five services

---
+++ src/micro-configure/src/main/java/com/example/microconfigure/MicroConfigureApplication.java
@@ -0,0 +1,15 @@
+package com.example.microconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MicroConfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroConfigureApplication.class, args);
+    }
+
+}
---
+++ src/micro-rating-service/src/test/java/com/example/microratingservice/MicroRatingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microratingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroRatingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
